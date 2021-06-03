97678857114261b973488e391ef3ddf7ad768437
Julian Archila <julialejo2018@gmail.com>
2021-06-02 19:23:03 -0500

    Login and Signup

--- api/users/serializers/users.py
+++ api/users/serializers/users.py
@@ -1,26 +1,36 @@
 """ User serializers. """
 
 # Django
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, password_validation
 
 # Django REST Framework
 from django.db.models import fields
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from rest_framework.authtoken.models import Token
 
 # Models
-from api.users.models import User 
+from api.users.models import User, Profile
+
 
 class UserModelSerializer(serializers.ModelSerializer):
     """ User model serializer. """
     class Meta:
         model = User
-        fields = "__all__"
+        fields = (
+            "email",
+            "username",
+            "first_name",
+            "last_name",
+            "is_staff",
+        )
+
 
 class UserLoginSerializer(serializers.Serializer):
     """ User login serializer. """
     email = serializers.EmailField()
-    password = serializers.CharField()
-    
+    password = serializers.CharField(min_length=8)
+
     def validate(self, data):
         user = authenticate(username=data["email"], password=data["password"])
         if not user:
@@ -30,5 +40,59 @@ class UserLoginSerializer(serializers.Serializer):
         return data
 
     def create(self, validated_data):
-        return self.context["user"]
+        user = self.context["user"]
+        token, created = Token.objects.get_or_create(user=user)
+        return user, token.key
 
+class UserSignUpSerializer(serializers.Serializer):
+    """ User signup serializer. 
+    Handles data validation and user/profile creation.
+    """
+    email = serializers.EmailField(
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message="A user with this email already exists."
+            )
+        ]
+    )
+    username = serializers.CharField(
+        min_length=2,
+        max_length=40,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message="This username is already taken"
+            )
+        ]
+    )
+
+    first_name = serializers.CharField(min_length=2, max_length=30)
+    last_name = serializers.CharField(min_length=2, max_length=30)
+
+    # Password 
+    password = serializers.CharField(min_length=8, max_length=200)
+    password_confirmation = serializers.CharField(min_length=8, max_length=200)
+
+    def validate(self, data):
+        password = data["password"]
+        password_confirmation = data["password_confirmation"]
+
+        if password != password_confirmation:
+            raise serializers.ValidationError("Passwords don't match.")
+
+        password_validation.validate_password(password=password)
+
+        return data
+
+    def create(self, data):
+        data.pop("password_confirmation")
+        # Create user
+        user = User.objects.create_user(**data, is_staff=False)
+
+        # Create user's objects
+        Profile.objects.create(user=user)
+
+        token, created = Token.objects.get_or_create(user=user)
+
+        return user, token.key
--- api/users/views/users.py
+++ api/users/views/users.py
@@ -8,10 +8,15 @@ from rest_framework.decorators import action
 from api.users import serializers
 
 # Serializers
-from api.users.serializers import UserLoginSerializer, UserModelSerializer
+from api.users.serializers import (
+    UserLoginSerializer,
+    UserModelSerializer,
+    UserSignUpSerializer
+)
 
 # Models
 from api.users.models import User
+
 
 class UserViewSet(GenericViewSet):
     """ User view set. """
@@ -19,11 +24,25 @@ class UserViewSet(GenericViewSet):
     serializer_class = UserModelSerializer
     lookup_field = "username"
 
-    @action(detail=False,methods=["POST"])
+    @action(detail=False, methods=["POST"])
     def login(self, request, *args, **kwargs):
         serializer = UserLoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
+        user, token = serializer.save()
+        data = {
+            "user": UserModelSerializer(user).data,
+            "token": token
+        }
+        return Response(data=data, status=status.HTTP_200_OK)
 
+    @action(detail=False, methods=["POST"])
+    def signup(self, request, *args, **kwargs):
+        serializer = UserSignUpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user, token = serializer.save()
+        data = {
+            "user": UserModelSerializer(user).data,
+            "token": token
+        }
+        return Response(data=data, status=status.HTTP_201_CREATED)
 
