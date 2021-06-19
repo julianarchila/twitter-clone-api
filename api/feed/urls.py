630eb97a9d7e6c0a84d2541384751f5e2fcc009b
Julian Archila <julialejo2018@gmail.com>
2021-06-19 17:07:40 -0500

    Profile feed and home feed improvement.

--- api/feed/urls.py
+++ api/feed/urls.py
@@ -4,8 +4,9 @@
 from django.urls import path
 
 # Views
-from api.feed.views import HomeFeed
+from api.feed.views import HomeFeed, ProfileFeed
 
 urlpatterns = [
     path("feed/home/", HomeFeed.as_view()),
+    path("feed/profile/", ProfileFeed.as_view()),
 ]
--- api/feed/views.py
+++ api/feed/views.py
@@ -1,14 +1,17 @@
 """ Feed views. """
 
 # Django REST Framework
-from django.utils.regex_helper import contains
+from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from api.tweets import serializers
 
 # Serializers
 from api.tweets.serializers import TweetSerializer
+
+# Models
+from api.users.models import User
+from api.tweets.models import Tweet
 
 
 class HomeFeed(GenericAPIView):
@@ -18,13 +21,37 @@ class HomeFeed(GenericAPIView):
 
     def get(self, request):
         user = request.user
-        tweets = list(user.tweets.all())
-        for p in user.following.all():
-            for t in p.user.tweets.all():
-                print(t)
-                tweets.append(t)
+        # Get all user tweets
+        tweets = user.tweets.all()
+
+        # Get tweets from users current user follows
+        following = user.following.all()
+        more_tweets = Tweet.objects.filter(user__profile__in=following)
+
+        # Mix tweets and order by most recent
+        tweets |= more_tweets
+        tweets = tweets.order_by("-created")
 
         serializer = TweetSerializer(
             tweets, many=True, context=self.get_serializer_context()
         )
-        return Response(data=serializer.data, status=200)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+class ProfileFeed(GenericAPIView):
+    "User profile feed view."
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        try:
+            user = User.objects.get(username=request.query_params.get("u"))
+        except User.DoesNotExist:
+            return Response(
+                data={"message": "user not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+        tweets = user.tweets.all().order_by("-created")
+        serializer = TweetSerializer(
+            tweets, many=True, context=self.get_serializer_context()
+        )
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
