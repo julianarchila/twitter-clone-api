bfd484da035bd27e6625962892499171badaa167
Julian Archila <julianarchilacaro@gmail.com>
2021-06-30 21:31:00 -0500

    Default profile pic.

--- api/users/models/profiles.py
+++ api/users/models/profiles.py
@@ -8,25 +8,25 @@ from api.utils.models import TwModel
 
 
 class Profile(TwModel):
-    """ Profile model. """
+    """Profile model."""
+
     user = models.OneToOneField("users.User", on_delete=models.CASCADE)
     picture = models.ImageField(
         "Profile picture",
         upload_to="users/pictures/",
+        default="users/pictures/default.png",
         blank=True,
-        null=True
+        null=True,
     )
     header = models.ImageField(
-        "Profile header",
-        upload_to="users/headers/",
-        blank=True,
-        null=True
+        "Profile header", upload_to="users/headers/", blank=True, null=True
     )
 
     bio = models.TextField(max_length=160, blank=True, null=True)
     followers_count = models.IntegerField(blank=True, null=True, default=0)
     followers = models.ManyToManyField(
-        "users.User", related_name='following', blank=True)
+        "users.User", related_name="following", blank=True
+    )
     """
     profile = UserProfile.objects.first()
     profile.followers.all() -> All users following this profile
